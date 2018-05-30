460583d0707e541452a208e9aeb41b469b04ad78
paperjuice <paperjuiceing@gmail.com>
2018-05-30 11:40:21 +0200

    Build app in one file

--- frontend/App/Main.elm
+++ frontend/App/Main.elm
@@ -11,7 +11,7 @@ main =
   { init = init
   , view = view
   , update = update
-  , subscriptions = subscriptions
+  , subscriptions = (\_-> Sub.none)
   }
 
 -- MSG --
@@ -29,25 +29,14 @@ init location =
 update : Msg -> Routing.Model -> (Routing.Model, Cmd msg)
 update msg model =
   case msg of
-    Route location ->
-      let
-          route = Routing.parseLocation location
-            |> Debug.log "location"
-      in
-          ({ model | route = route }, Cmd.none)
 
 
 -- VIEW --
 view : Routing.Model -> Html msg
 view model =
   case model.route of
-    Routing.Home -> 
-      Home.view Home.Model
-
-    _    -> div [] [ text "Else" ]
-
 
 -- SUBSCRIPTIONS --
-subscriptions : Routing.Model -> Sub msg
-subscriptions model =
-  Sub.none
+--subscriptions : Routing.Model -> Sub msg
+--subscriptions model =
+--  Sub.none

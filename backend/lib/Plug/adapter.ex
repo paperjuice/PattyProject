6e2c95d899ecb0347256fa5de2c796187c41ca6d
paperjuice <paperjuiceing@gmail.com>
2018-06-10 12:25:00 +0300

    Remove screenshots

--- backend/lib/Plug/adapter.ex
+++ backend/lib/Plug/adapter.ex
@@ -1,11 +1,20 @@
 defmodule Backend.Plug.Adapter do
   use Plug.Router
 
+  plug Plug.Parsers, parsers: [:json],
+                   pass: ["text/*"],
+                   json_decoder: Poison
   plug :match
   plug :dispatch
 
+
   get "/" do
     send_resp(conn, 200, "hello")
+  end
+
+  post "/register" do
+    IO.inspect(conn, label: Hell0)
+    send_resp(conn, 200, "hey")
   end
 
   match _ do
